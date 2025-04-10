afb8e05947afb6590b00528d888dc77981194e21
Annaluo <86500018+Annaluo95@users.noreply.github.com>
2025-04-10 09:05:53 -0700

    Add files via upload

---
+++ Chapter 1-Exercise 2.py
@@ -0,0 +1,30 @@
+## CIS340 - Chapter 1 Exercise 2 - Yinfang Luo
+#  Sample Program that demonstrates the print function
+#  The following line prints 7
+print(3 + 4)
+#  Print Hello World! on two lines
+print("Hello")
+print("World!")
+print("\n")
+print("Hello" "\n" "World!")
+#  Print multiple values with a single print function call
+print("My favorite number are", 3+4, "and", 3+10)
+print("This is the first line\nNow the second line!")
+print("Goodbye")
+print()
+print("Hope to see you again")
+'''
+7
+Hello
+World!
+
+
+Hello
+World!
+My favorite number are 7 and 13
+This is the first line
+Now the second line!
+Goodbye
+
+Hope to see you again
+'''
